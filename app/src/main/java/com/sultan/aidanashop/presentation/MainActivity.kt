ce81b11bc6b947c51176fb37f037b903db821c67
Sultan-assasin <sulltanadai@gmail.com>
2022-10-28 23:31:02 +0600

    Initial commit

---
+++ app/src/main/java/com/sultan/aidanashop/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sultan.aidanashop.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sultan.aidanashop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
